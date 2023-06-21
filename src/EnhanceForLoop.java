d864d8c6f5657136d4df0a73fffd35d957bcc514
illson97 <illson97@github.com>
2023-06-21 16:58:05 +0900

    두잇 자바 ~chapter7

---
+++ src/EnhanceForLoop.java
@@ -0,0 +1,9 @@
+public class EnhanceForLoop {
+    public static void main(String[ ] args) {
+        String[ ] strArray = {"JAVA", "ANDROID", "C", "JAVASCRIPT", "PYTHON"};
+
+        for(String lang : strArray) {
+            System.out.println(lang);
+        }
+    }
+}
