f4d262ff750605260669401bf6e6611d443e8f2b
illson97 <illson97@github.com>
2023-06-26 17:59:44 +0900

    TIL2_JAVA&SPRING_6th

---
+++ TIL2_JAVA_6th/src/Queue.java
@@ -0,0 +1,5 @@
+public interface Queue {
+    void enQueue(String title);
+    String deQueue();
+    int getSize();
+}
---
+++ TIL2_JAVA_6th/src/Sell.java
@@ -0,0 +1,7 @@
+public interface Sell {
+    void sell();
+
+    default void order() {
+        System.out.println("판매 주문");
+    }
+}
