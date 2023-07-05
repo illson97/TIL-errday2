1e56f9532ea116edb1c29b3e0a196551369a841d
illson97 <illson97@github.com>
2023-07-05 18:17:44 +0900

    TIL_JAVA_SPRING_13th

--- learn-jpa-and-hibernate/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/Course.java
+++ learn-jpa-and-hibernate/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/Course.java
@@ -1,10 +1,23 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
+
+    @Id
     private long id;
+    @Column(name="name")
     private String name;
+
+    @Column(name="author")
     private String author;
 
+    public Course() {
+
+    }
     public Course(long id, String name, String author) {
         this.id = id;
         this.name = name;
---
+++ myfirstwepapp/myfirstwepapp/src/main/java/com/in28minutes/springboot/myfirstwepapp/MyfirstwepappApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.myfirstwepapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwepappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwepappApplication.class, args);
+	}
+
+}
